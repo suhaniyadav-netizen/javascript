c478d7a3082ea1327296658c62666c1483b92e7b
suhaniyadav-netizen <suhani.yadavmicro@gmail.com>
2026-01-21 12:03:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+// let obj = {
+//     name : "Suhani",
+//     address : "gurgaon",
+//     age:18
+// };
+
+// obj.age= 19    // to update the value 
+// console.log(obj);
+
+// const para = document.querySelector("p")  // returns only the first matching element 
+// para.innerText = "Hello World!"
+// para.style.color = "blue"
+// console.log(para);
+
+// const para = document.getElementsByClassName("para")  // returns all the matching elements in the form of HTML collection
+// para[0].innerText = "Hello World!"
+// para[0].style.color = "blue"
+// console.log(para);
+
+// const para2 = document.querySelectorAll(".para")  // returns all the matching elements in the form of NodeList
+// para2[0].innerText = "Hello World!"
+// para2[0].style.color = "blue"
+// console.log(para2);
+
+// let arr = [23,54,46,232,564,23]
+
+// arr.forEach((ele)=> console.log(ele))
+// console.log(arr);
+
+// const container = document.querySelector(".container")
+// container.innerHTML = "<h1>This is container</h1>"
+// console.log(container)
+
+// function message()
+// {    alert("Button Clicked")
+// }
+const btn = document.querySelector("button")
+btn.classList.add("btn")
+// btn.addEventListener("click", message)      // click type of event
+// btn.removeEventListener("click",message)
+// btn.addEventListener("mouseover", message)  // mouseover type of event 
+// btn.addEventListener("keyup", message)      // keyup type of event
+// btn.addEventListener("keydown", message)   // keydown type of event
+
+// const form = document.querySelector("form")
+// form.addEventListener("submit", function(event){
+//     event.preventDefault()   // to prevent the default behaviour of form submission
+//     console.log("Form Submitted")
+// })
+
+const container=document.querySelector(".container")
+const outer=document.querySelector(".outer")
+const button=document.querySelector("button")
+
+container.addEventListener("click", ()=>{console.log("Div")} , true)      // capturing phase
+outer.addEventListener("click", ()=>{console.log("Outer Div")} , true) 
+button.addEventListener("click", ()=>{console.log("Button")} , true) 
+
+// container.addEventListener("click", ()=>{ console.log("Div")})     // bubbling phase by default false 
+// outer.addEventListener("click", ()=>{ console.log("Outer Div")}) 
+// button.addEventListener("click", ()=>{ console.log("Button")}) 
